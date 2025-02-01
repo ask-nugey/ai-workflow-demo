f244103694f8687aedbcd9d1823e93f8b7f615fa
ask-nugey <yuki.iwase@blueish.co.jp>
2025-02-02 02:02:22 +0900

    first commit

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import os
+
+import streamlit as st
+from dotenv import load_dotenv
+from openai import OpenAI
+
+from prompt.instructions import SYSTEM_INSTRUCTIONS
+
+USER_NAME = "user"
+ASSISTANT_NAME = "assistant"
+
+# 環境変数の読み込みとクライアントの初期化
+load_dotenv()
+client = OpenAI()
+
+def main() -> None:
+    st.title("Hello, World!")
+
+if __name__ == "__main__":
+    main()
